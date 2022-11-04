dd5a54bc9b97a1e28052f90744e68095b7ff95fa
Josh <lutz_143@yahoo.com>
2022-11-04 16:08:07 -0400

    create iterator function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,48 +3,24 @@ const splitLog = '==========================='
 // Create and array of all the password characters we can use
 const passwordCharacters = [
     '@','%','+','\\','/',"'",'!','#','$','^','?',
-    ':',',',')','(','}','{',']','[','~','-','_','.',];
+    ':',',',')','(','}','{',']','[','~','-','_','.',
+    'a','b','c','d','e','f','g','h','i','j','k','l','m',
+    'n','o','p','q','r','s','t','u','v','w','x','y','z',
+    'A','B','C','D','E','F','G','H','I','J','K','L','M',
+    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-for(var i=0; i < passwordCharacters.length; i++) {
-  // This statement will run each time the loop is executed
-  console.log(passwordCharacters[i]);
+iteratorFun(passwordCharacters)
+
+
+
+
+function iteratorFun(x){
+  for(var i=0; i < x.length; i++) {
+    // This statement will run each time the loop is executed
+    console.log(x[i]);
+  }  
+  console.log(splitLog);
 }
-console.log(splitLog);
-
-// Create an array of numbers as a string for selection
-const passwordNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-for(var i=0; i < passwordNumbers.length; i++) {
-  // This statement will run each time the loop is executed
-  console.log(passwordNumbers[i]);
-}
-console.log(splitLog);
-
-// Create an array of lower case letters for selection
-const passwordLowerCase = [
-  'a','b','c','d','e','f','g','h','i','j','k','l','m',
-  'n','o','p','q','r','s','t','u','v','w','x','y','z',];
-
-for(var i=0; i < passwordLowerCase.length; i++) {
-  // This statement will run each time the loop is executed
-  console.log(passwordLowerCase[i]);
-}  
-console.log(splitLog);
-
-// Create and array of upper case letters for selection
-const passwordUpperCase = [
-  'A','B','C','D','E','F','G','H','I','J','K','L','M',
-  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
-
-for(var i=0; i < passwordUpperCase.length; i++) {
-  // This statement will run each time the loop is executed
-  console.log(passwordUpperCase[i]);
-}  
-console.log(splitLog);
-
-
-
-
 
 
 
